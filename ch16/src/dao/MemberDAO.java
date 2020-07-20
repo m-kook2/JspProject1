97b76f61ef90e1d17da8fcaf80150a752f1f63bc
user <user@DESKTOP-IFOHMFV>
2020-07-20 20:22:39 +0900

    추가

---
+++ ch16/src/dao/MemberDAO.java
@@ -0,0 +1,83 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+public class MemberDAO {
+	private static MemberDAO instance;
+
+	private MemberDAO() {
+	}
+
+	public static MemberDAO getInstance() {
+		if (instance == null) {
+			instance = new MemberDAO();
+		}
+		return instance;
+	}
+
+	private Connection getConnection() {
+		Connection conn = null;
+		try {
+			Context ctx = new InitialContext();
+			DataSource ds = (DataSource) ctx.lookup("java:comp/env/jdbc/OracleDB");
+			conn = ds.getConnection();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		return conn;
+	}
+
+	public int insert(MemberVO memberVO) throws SQLException {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		int result = 0;
+		ResultSet rs = null;
+		int number = 0;
+		String sql1 = "select nvl(max(idx),0) from member";
+		String sql = "insert into member values(?,?,?,?,?,?,sysdate)";
+		try {
+			conn = getConnection();
+			pstmt = conn.prepareStatement(sql1);
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				number = rs.getInt(1) + 1;
+			}else {
+				if(number==0) {
+					number = 1;
+				}
+			}
+			rs.close();
+			pstmt.close();
+			
+			if (number != 0) {
+				pstmt = conn.prepareStatement(sql);
+				pstmt.setInt(1, number);
+				pstmt.setString(2, memberVO.getId());
+				pstmt.setString(3, memberVO.getEmail());
+				pstmt.setString(4, memberVO.getPassword());
+				pstmt.setString(5, memberVO.getNickname());
+				pstmt.setString(6, memberVO.getGender());
+				result = pstmt.executeUpdate();
+				pstmt.close();
+			}
+				
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		} finally {
+			if (rs != null)
+				rs.close();
+			if (pstmt != null)
+				pstmt.close();
+			if (conn != null)
+				conn.close();
+		}
+		return result;
+	}
+}
