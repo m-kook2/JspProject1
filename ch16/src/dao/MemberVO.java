3be6afe6d733cc66092d7e146fb010ea576cc4cd
LG <LG@DESKTOP-MUK8P2D>
2020-07-21 09:12:41 +0900

    업데이트

---
+++ ch16/src/dao/MemberVO.java
@@ -0,0 +1,67 @@
+package dao;
+
+public class MemberVO {
+	private int idx;
+	private String id;
+	private String email;
+	private String password;
+	private String nickname;
+	private String gender;
+	private String reg_date;
+
+	public int getIdx() {
+		return idx;
+	}
+
+	public void setIdx(int idx) {
+		this.idx = idx;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getReg_date() {
+		return reg_date;
+	}
+
+	public void setReg_date(String reg_date) {
+		this.reg_date = reg_date;
+	}
+}
