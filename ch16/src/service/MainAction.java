88c6e57a26f11756549e1afbfd39ff0cf955d065
LG <LG@DESKTOP-MUK8P2D>
2020-07-29 19:41:22 +0900

    tt

--- ch16/src/service/MainAction.java
+++ ch16/src/service/MainAction.java
@@ -7,17 +7,18 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import dao.MainDao;
 import dao.MovieDto;
 public class MainAction implements CommandProcess {
 	public String requestPro(HttpServletRequest request,
 		HttpServletResponse response) throws ServletException, IOException {
-		/*MovieDao dao=MovieDao.getInstance();
+		MainDao dao=MainDao.getInstance();
 		try {
-			List<MovieDto> list=dao.selectMovie();
+			List<MovieDto> list=dao.list();
 			request.setAttribute("list", list);
 		} catch (SQLException e) {
 			e.printStackTrace();
-		}*/
+		}
 		return "main/main.jsp";
 	}
 }
