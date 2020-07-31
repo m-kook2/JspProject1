0ad0e331238fd4d24ee15108f3b6ea043aabe2a7
user <user@DESKTOP-9LNCCK4>
2020-07-31 11:54:26 +0900

    수정

---
+++ ch16/src/dao/CSDto.java
@@ -0,0 +1,50 @@
+package dao;
+
+import java.util.Date;
+
+public class CSDto {
+	private int num;
+	private String writer;
+	private String subject;
+	private String content;
+	private String passwd;
+	private Date reg_date;
+	
+	
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getPasswd() {
+		return passwd;
+	}
+	public void setPasswd(String passwd) {
+		this.passwd = passwd;
+	}
+	public Date getReg_date() {
+		return reg_date;
+	}
+	public void setReg_date(Date reg_date) {
+		this.reg_date = reg_date;
+	}
+}
