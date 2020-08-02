6169e57fe87c8612e4906df85b5c98bdbf3becb1
user <user@DESKTOP-IFOHMFV>
2020-08-03 08:49:14 +0900

    sql문 추가

--- ch16/WebContent/META-INF/익스포트.sql
+++ ch16/WebContent/META-INF/익스포트.sql
@@ -285,9 +285,9 @@ Insert into J20200701.COMM (C_IDX,ID,C_CONTENT,C_SYMPATHY,C_UNSYMPATHY,C_GRADE,C
 Insert into J20200701.COMM (C_IDX,ID,C_CONTENT,C_SYMPATHY,C_UNSYMPATHY,C_GRADE,C_DATE,DEL_YN,M_IDX,STEP,DEP) values (10,'hongil3','여섯번째 게시물 댓글',100,20,8,to_date('20/07/28','RR/MM/DD'),'n',6,0,0);
 REM INSERTING into J20200701.MEMBER
 SET DEFINE OFF;
-Insert into J20200701.MEMBER (ID,IDX,PASSWORD,EMAIL,NICKNAME,GENDER,REG_DATE,DEL_YN,STATUS,PIC) values ('admin',1,'2468','email1@gmail.com','nickname2','남자','2020/07/01','N','2','');
-Insert into J20200701.MEMBER (ID,IDX,PASSWORD,EMAIL,NICKNAME,GENDER,REG_DATE,DEL_YN,STATUS,PIC) values ('hongil2',2,'2468','email2@gmail.com','nickname3','여자','2010/07/01','N','1','');
-Insert into J20200701.MEMBER (ID,IDX,PASSWORD,EMAIL,NICKNAME,GENDER,REG_DATE,DEL_YN,STATUS,PIC) values ('hongil3',3,'2468','email4@3gmail.com','nickname4','남자','2030/07/01','N','1','');
+Insert into J20200701.MEMBER (ID,IDX,PASSWORD,EMAIL,NICKNAME,GENDER,REG_DATE,DEL_YN,STATUS,PIC) values ('admin',1,'2468','email1@gmail.com','nickname2','남자','2020/07/01','N','2','who.jpg');
+Insert into J20200701.MEMBER (ID,IDX,PASSWORD,EMAIL,NICKNAME,GENDER,REG_DATE,DEL_YN,STATUS,PIC) values ('hongil2',2,'2468','email2@gmail.com','nickname3','여자','2010/07/01','N','1','mangu.jpg');
+Insert into J20200701.MEMBER (ID,IDX,PASSWORD,EMAIL,NICKNAME,GENDER,REG_DATE,DEL_YN,STATUS,PIC) values ('hongil3',3,'2468','email4@3gmail.com','nickname4','남자','2030/07/01','N','1','chulsu.jpg');
 Insert into J20200701.MEMBER (ID,IDX,PASSWORD,EMAIL,NICKNAME,GENDER,REG_DATE,DEL_YN,STATUS,PIC) values ('hongil4',4,'2468','email5@gmail.com','nickname6','남자','2019/07/01','N','1','');
 Insert into J20200701.MEMBER (ID,IDX,PASSWORD,EMAIL,NICKNAME,GENDER,REG_DATE,DEL_YN,STATUS,PIC) values ('hongil5',5,'2468','email6@gmail.com','nickname7','여자','2018/07/01','N','1','');
 Insert into J20200701.MEMBER (ID,IDX,PASSWORD,EMAIL,NICKNAME,GENDER,REG_DATE,DEL_YN,STATUS,PIC) values ('hongil6',6,'2468','email6@gmail.com','nickname9','남자','2021/07/01','N','3','');
